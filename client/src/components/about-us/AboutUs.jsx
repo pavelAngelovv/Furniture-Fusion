5e79d29d9481ae637bd8b78f6b18f1f06aa9e83b
Pavel Angelov <pangelov@codixis.com>
2024-07-31 15:32:52 +0300

    Fix import

--- client/src/components/about-us/AboutUs.jsx
+++ client/src/components/about-us/AboutUs.jsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/BContainerox';
+import Container from '@mui/material/Container';
 
 function AboutUs() {
   return (
