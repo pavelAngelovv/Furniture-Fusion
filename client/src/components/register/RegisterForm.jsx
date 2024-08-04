e56775869c33adda38e872d0c08fa549b34e7f40
Pavel Angelov <pangelov@codixis.com>
2024-08-04 14:43:10 +0300

    Update register form layout and add location field using external api

--- client/src/components/register/RegisterForm.jsx
+++ client/src/components/register/RegisterForm.jsx
@@ -4,14 +4,18 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Autocomplete from '@mui/material/Autocomplete';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { login } from '../../slices/authSlice';
+import { useState } from 'react';
 
 const RegisterForm = () => {
     const { register, handleSubmit, formState: { errors }, watch } = useForm();
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [locationOptions, setLocationOptions] = useState([]);
 
     const onSubmit = async (data) => {
         try {
@@ -22,6 +26,17 @@ const RegisterForm = () => {
         } catch (error) {
             console.error('Registration failed:', error.response?.data || error.message);
             alert(error.response?.data.message);
+        }
+    };
+
+    const handleLocationChange = async (event, value) => {
+        if (value) {
+            try {
+                const response = await axios.get(`https://api.geoapify.com/v1/geocode/autocomplete?text=${value}&apiKey=356aca8cace74bb6808de9646c2d3861`);
+                setLocationOptions(response.data.features.map(place => place.properties.formatted));
+            } catch (error) {
+                console.error('Error fetching location suggestions:', error);
+            }
         }
     };
 
@@ -43,97 +58,124 @@ const RegisterForm = () => {
                 Register
             </Typography>
 
-            <TextField
-                fullWidth
-                margin="normal"
-                id="firstName"
-                label="First Name"
-                {...register('firstName', { required: 'First name is required' })}
-                error={!!errors.firstName}
-                helperText={errors.firstName?.message}
-            />
-
-            <TextField
-                fullWidth
-                margin="normal"
-                id="lastName"
-                label="Last Name"
-                {...register('lastName', { required: 'Last name is required' })}
-                error={!!errors.lastName}
-                helperText={errors.lastName?.message}
-            />
-
-            <TextField
-                fullWidth
-                margin="normal"
-                id="email"
-                label="Email"
-                type="email"
-                {...register('email', {
-                    required: 'Email is required',
-                    pattern: {
-                        value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-                        message: 'Email is not valid',
-                    },
-                })}
-                error={!!errors.email}
-                helperText={errors.email?.message}
-            />
-
-            <TextField
-                fullWidth
-                margin="normal"
-                id="password"
-                label="Password"
-                type="password"
-                {...register('password', {
-                    required: 'Password is required',
-                    minLength: {
-                        value: 6,
-                        message: 'Password must be at least 6 characters long',
-                    },
-                })}
-                error={!!errors.password}
-                helperText={errors.password?.message}
-            />
-
-            <TextField
-                fullWidth
-                margin="normal"
-                id="confirmPassword"
-                label="Confirm Password"
-                type="password"
-                {...register('confirmPassword', {
-                    required: 'Please confirm your password',
-                    validate: value => value === password || 'Passwords do not match',
-                })}
-                error={!!errors.confirmPassword}
-                helperText={errors.confirmPassword?.message}
-            />
-
-            <TextField
-                fullWidth
-                margin="normal"
-                id="phoneNumber"
-                label="Phone Number"
-                type="tel"
-                {...register('phoneNumber', {
-                    required: 'Phone number is required',
-                    pattern: {
-                        value: /^[0-9]{10}$/,
-                        message: 'Phone number must be 10 digits',
-                    },
-                })}
-                error={!!errors.phoneNumber}
-                helperText={errors.phoneNumber?.message}
-            />
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        id="firstName"
+                        label="First Name"
+                        {...register('firstName', { required: 'First name is required' })}
+                        error={!!errors.firstName}
+                        helperText={errors.firstName?.message}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        id="lastName"
+                        label="Last Name"
+                        {...register('lastName', { required: 'Last name is required' })}
+                        error={!!errors.lastName}
+                        helperText={errors.lastName?.message}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        id="email"
+                        label="Email"
+                        type="email"
+                        {...register('email', {
+                            required: 'Email is required',
+                            pattern: {
+                                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                                message: 'Email is not valid',
+                            },
+                        })}
+                        error={!!errors.email}
+                        helperText={errors.email?.message}
+                    />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        id="phoneNumber"
+                        label="Phone Number"
+                        type="tel"
+                        {...register('phoneNumber', {
+                            required: 'Phone number is required',
+                            pattern: {
+                                value: /^[0-9]{10}$/,
+                                message: 'Phone number must be 10 digits',
+                            },
+                        })}
+                        error={!!errors.phoneNumber}
+                        helperText={errors.phoneNumber?.message}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Autocomplete
+                        freeSolo
+                        options={locationOptions}
+                        onInputChange={handleLocationChange}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                fullWidth
+                                margin="normal"
+                                id="location"
+                                label="Location"
+                                {...register('location', { required: 'Location is required' })}
+                                error={!!errors.location}
+                                helperText={errors.location?.message}
+                            />
+                        )}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        id="password"
+                        label="Password"
+                        type="password"
+                        {...register('password', {
+                            required: 'Password is required',
+                            minLength: {
+                                value: 6,
+                                message: 'Password must be at least 6 characters long',
+                            },
+                        })}
+                        error={!!errors.password}
+                        helperText={errors.password?.message}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        fullWidth
+                        margin="normal"
+                        id="confirmPassword"
+                        label="Confirm Password"
+                        type="password"
+                        {...register('confirmPassword', {
+                            required: 'Please confirm your password',
+                            validate: value => value === password || 'Passwords do not match',
+                        })}
+                        error={!!errors.confirmPassword}
+                        helperText={errors.confirmPassword?.message}
+                    />
+                </Grid>
+            </Grid>
 
             <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mt: 2 }}>
                 <Button
                     type="submit"
                     variant="contained"
                     color="primary"
-                    sx={{mr: '30px'}}
                 >
                     Register
                 </Button>
