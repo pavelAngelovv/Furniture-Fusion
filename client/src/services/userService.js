cc9df6e70062cc5b1cbc3c15e5b36f140a55f123
Pavel Angelov <pangelov@codixis.com>
2024-08-07 18:48:50 +0300

    Update env variable and services

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL + 'users';
+const API_URL = import.meta.env.VITE_API_URL + '/users';
 
 export const getUserData = async () => {
     const token = localStorage.getItem('accessToken');
