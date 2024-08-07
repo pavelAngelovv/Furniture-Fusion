583945f3148a019212d4aa7486399a24816785bd
Pavel Angelov <pangelov@codixis.com>
2024-08-07 18:36:21 +0300

    Update env variable and services

--- client/src/services/furnitureService.js
+++ client/src/services/furnitureService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL + '/data/furniture';
+const API_URL = import.meta.env.VITE_API_URL + 'data/furniture';
 
 export const getFurnitureItems = async () => {
   const response = await axios.get(API_URL);
