d0531dbf56ac10c87f108af4ba349ddbb5a994b2
Pavel Angelov <pangelov@codixis.com>
2024-08-07 18:15:59 +0300

    Update .env

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3030/users'
+const API_URL = import.meta.env.VITE_API_URL + '/users';
 
 export const getUserData = async () => {
     const token = localStorage.getItem('accessToken');
